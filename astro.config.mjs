4730ef9dc31a0a985353381abddf929f0b9f009d
jaroseror <jaroseror@unal.edu.co>
2025-12-03 15:42:53 -0500

    preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,3 +14,11 @@ export default defineConfig({
     alpinejs()
   ]
 });
+
+// preact integration
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+export default defineConfig({
+  integrations: [preact()]
+});
