41261ba4fa035e05c0d1d3dd7f06dd899067d211
Nicolas <nguiom@unal.edu.co>
2025-12-10 15:54:23 -0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://JoseAlejandroRosero.github.io',
+  site: 'https://nguiom.github.io/test.github.io',
   base: '\Los4Automaticos',
   vite: {
     plugins: [tailwindcss()],
